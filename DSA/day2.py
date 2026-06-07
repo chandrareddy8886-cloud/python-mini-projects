e4750ed520a6ad377207d870216b6d8e9c4366c2
chandrareddy8886 <chandrareddy8886@gmail.com>
2026-06-07 22:09:12 +0530

    Add DSA Day 1 and Day 2

---
+++ DSA/day2.py
@@ -0,0 +1,32 @@
+# 1)print 1 to 10 numbers
+"""
+for i in range(1,11):
+    print(i)"""
+
+# 2) print 10 to 1
+"""for i in range(10,0,-1):
+    print(i)"""
+
+# 3) print even numbers
+"""
+for i in range(0,21,2):
+    print(i)"""
+
+# 4)print odd numbers
+
+"""for i in range(1,20,2):
+    print(i)"""
+
+# 5)find sum
+'''numbers = [1, 2, 3, 4, 5]
+total=0
+for i in numbers:
+    total=total+i
+print(total)'''
+
+# 6)find number of elements
+numbers = [5, 10, 15, 20]
+count=0
+for i in numbers:
+    count=count+1
+print(count)
